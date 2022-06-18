e82e98051315b1a67f632f5c798ba6b38b043624
Mbartlett413 <Mbartlett413@me.com>
2022-06-18 03:26:09 -0500

    display image

--- src/app/game/index/index.component.ts
+++ src/app/game/index/index.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GameService } from '../game.service';
 import { Game } from '../game';
+import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-index',
@@ -15,23 +17,26 @@ export class IndexComponent implements OnInit {
   ip_address: String = "";
   selected_id: number = 0;
   game_likes: String = "";
+  image_path: any;
 
 
-  constructor(public gameService: GameService) { }
+  constructor(public gameService: GameService,
+              private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.gameService.getAll().subscribe(data => {
       this.games = data;
-      this.game = data[0];
     });
   }
 
   showGame(id: number) {
     this.gameService.find(id).subscribe(data => {
+      console.log(data)
       this.display_show = true;
       this.game = data.game;
       this.game_likes = data.likes;
       this.selected_id = data.game.id;
+      this.image_path = 'data:image/png;base64, ' + data.image ;
     });
   }
 
@@ -45,5 +50,9 @@ export class IndexComponent implements OnInit {
 
   toggleModal(){
     this.display_show = !this.display_show
+  }
+
+  transform(){
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.image_path);
   }
 }
